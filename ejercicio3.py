9cefc026a176864c1a785c8d6b08706a9bb18be2
KamilaGuzman01 <diany.guzman01@usa.edu.co>
2025-02-17 07:34:44 -0500

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,11 @@
+a:int = 10
+
+print(f"Numero inicial: {a}")
+a+=int(5)
+print(f"Aumentamos en 5: {a}")
+a-=int(3)
+print(f"Restamos en 3: {a}")
+a*=int(2)
+print(f"Multiplicamos por 2: {a}")
+a/=int(4)
+print(f"y por ultimo dividimos entre 4: {a}")
